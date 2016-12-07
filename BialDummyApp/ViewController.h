a73ec1750ee90b9ff94b088bed4ff01fb922bb3d
Shubhamay Das <shubhamd@thoughtworks.com>
2016-12-07 09:44:10 +0530

    Initial Commit

---
+++ BialDummyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BialDummyApp
+//
+//  Created by shubhamb on 12/7/16.
+//  Copyright © 2016 ThouhgtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BialDummyAppTests/BialDummyAppTests.m
@@ -0,0 +1,39 @@
+//
+//  BialDummyAppTests.m
+//  BialDummyAppTests
+//
+//  Created by shubhamb on 12/7/16.
+//  Copyright © 2016 ThouhgtWorks. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BialDummyAppTests : XCTestCase
+
+@end
+
+@implementation BialDummyAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
